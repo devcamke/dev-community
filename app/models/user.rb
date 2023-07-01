11e9957ddf3a1c914e2731114c11b43f4cb6dab8
Dennis Kamau <devjam.ke@gmail.com>
2023-07-01 18:10:44 +0300

    Use Faker gem to generate dummy data for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+PROFILE_TITLE=[
+  'Junior Ruby On Rails Developer',
+  'Senior Ruby On Rails Developer',
+  'Junior Full Stack Ruby On Rails Developer',
+  'Senior Full Stack Ruby On Rails Developer',
+  'Senior Full Stack Developer',
+  'Senior Front End Developer',
+  'Senior Java Developer',
+  'Ruby Developer',
+  'Senior Python Developer',
+  'Senior JavaScript Developer',
+  'Senior User Experience Developer',
+  'Senior Program Manager',
+  'Junior Java Developer'
+].freeze
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_01_134033) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_01_150526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_134033) do
     t.text "about"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "contact_number"
+    t.string "country"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
