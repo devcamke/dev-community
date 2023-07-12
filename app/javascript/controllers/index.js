6f0632c3d4ab6ce09da176ca0d9948c2615473d6
Dennis Kamau <devjam.ke@gmail.com>
2023-07-12 13:27:58 +0300

    Working with stimulus JS

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import UserNameController from "./user_name_controller"
+application.register("user-name", UserNameController)
